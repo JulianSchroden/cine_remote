7e784e93ed4355f04ffcb9c69742c60d42eee818
Julian Schroden <juschroden@gmail.com>
2022-12-25 01:45:11 +0100

    WIP create camera control layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:cine_remote/data/models/wifi_camera_handle.dart';
@@ -36,6 +37,8 @@ class _MyHomePageState extends State<MyHomePage> {
   CameraRemoteService cameraRemoteService = CameraRemoteService();
   WifiCameraHandle? handle;
   Uint8List? imageBytes;
+  bool isRecording = false;
+  Timer? _timer;
 
   @override
   void initState() {
@@ -44,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
-  void _getNetworkInfo() async {
+  void _connect() async {
     final result = await cameraRemoteService.connect();
 
     setState(() {
@@ -52,121 +55,213 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _triggerRecord() async {
+    await cameraRemoteService.triggerRecord(handle!);
+
+    setState(() {
+      isRecording = !isRecording;
+    });
+  }
+
   void _getLiveViewImage() async {
+    final stopwatch = Stopwatch();
+    stopwatch.start();
     final bytes = await cameraRemoteService.getLiveViewImage(handle!);
+    stopwatch.stop();
+    print(stopwatch.elapsedMilliseconds);
 
     setState(() {
       imageBytes = bytes;
     });
   }
 
+  void _startLiveView() async {
+    cameraRemoteService.startLiveView(handle!);
+    _timer?.cancel();
+    _timer = Timer.periodic(const Duration(milliseconds: 100), (timer) async {
+      final stopwatch = Stopwatch();
+      stopwatch.start();
+      final bytes = await cameraRemoteService.getLiveViewImage(handle!);
+      stopwatch.stop();
+      //print(stopwatch.elapsed);
+      print(stopwatch.elapsedMilliseconds);
+
+      setState(() {
+        imageBytes = bytes;
+      });
+    });
+  }
+
+  void _stopLiveView() async {
+    _timer?.cancel();
+    cameraRemoteService.stopLiveView(handle!);
+  }
+
+  Widget _buildActions() => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Connection State:',
+                  style: TextStyle(color: Colors.white)),
+              const SizedBox(width: 8),
+              Text(handle?.cookies.join() ?? 'not connected',
+                  style: const TextStyle(color: Colors.white)),
+            ],
+          ),
+          MaterialButton(
+            onPressed: _connect,
+            child: const Text(
+              'Connect to Camera',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          MaterialButton(
+            onPressed: () => cameraRemoteService.triggerRecord(handle!),
+            child: const Text(
+              'Trigger Record',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          MaterialButton(
+            onPressed: () => cameraRemoteService.setIso(handle!, '25600'),
+            child: const Text(
+              'Set ISO',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          /*
+                    MaterialButton(
+                      onPressed: () => cameraRemoteService.getIso(handle!),
+                      child: const Text(
+                        'Get ISO',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                      MaterialButton(
+                        onPressed: () => cameraRemoteService.getAperture(handle!),
+                        child: const Text(
+                          'Get Aperture',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      MaterialButton(
+                        onPressed: () => cameraRemoteService.getShutterAngle(handle!),
+                        child: const Text(
+                          'Get ShutterAngle',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      MaterialButton(
+                        onPressed: () => cameraRemoteService.getWhiteBalance(handle!),
+                        child: const Text(
+                          'Get WhiteBalance',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      */
+          MaterialButton(
+            onPressed: () => _startLiveView(),
+            child: const Text(
+              'Start LiveView',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          MaterialButton(
+            onPressed: () => _stopLiveView(),
+            child: const Text(
+              'Stop LiveView',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          MaterialButton(
+            onPressed: () => _getLiveViewImage(),
+            child: const Text(
+              'Get LiveView Image',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
+    const settingsSpacer = 32.0;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SizedBox(
-        width: double.infinity,
-        child: Stack(
+      body: SafeArea(
+        top: false,
+        bottom: false,
+        child: Row(
           children: [
-            if (imageBytes != null)
-              Container(
-                color: Colors.black,
-                child: Image.memory(
-                  imageBytes!,
-                  width: screenSize.width,
-                  height: screenSize.height,
-                  fit: BoxFit.contain,
-                ),
-              ),
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Connection State:',
-                        style: TextStyle(color: Colors.white)),
-                    const SizedBox(width: 8),
-                    Text(handle?.cookies.join() ?? 'not connected',
-                        style: const TextStyle(color: Colors.white)),
-                  ],
-                ),
-                MaterialButton(
-                  onPressed: _getNetworkInfo,
-                  child: const Text(
-                    'Connect to Camera',
-                    style: TextStyle(color: Colors.white),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.close),
+                    color: Colors.white,
                   ),
                 ),
-                MaterialButton(
-                  onPressed: () => cameraRemoteService.triggerRecord(handle!),
-                  child: const Text(
-                    'Trigger Record',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                MaterialButton(
-                  onPressed: () => cameraRemoteService.setIso(handle!, '25600'),
-                  child: const Text(
-                    'Set ISO',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                /*
-              MaterialButton(
-                onPressed: () => cameraRemoteService.getIso(handle!),
-                child: const Text(
-                  'Get ISO',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-                MaterialButton(
-                  onPressed: () => cameraRemoteService.getAperture(handle!),
-                  child: const Text(
-                    'Get Aperture',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                MaterialButton(
-                  onPressed: () => cameraRemoteService.getShutterAngle(handle!),
-                  child: const Text(
-                    'Get ShutterAngle',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                MaterialButton(
-                  onPressed: () => cameraRemoteService.getWhiteBalance(handle!),
-                  child: const Text(
-                    'Get WhiteBalance',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                */
-                MaterialButton(
-                  onPressed: () => cameraRemoteService.startLiveView(handle!),
-                  child: const Text(
-                    'Start LiveView',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                MaterialButton(
-                  onPressed: () => cameraRemoteService.stopLiveView(handle!),
-                  child: const Text(
-                    'Stop LiveView',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                MaterialButton(
-                  onPressed: () => _getLiveViewImage(),
-                  child: const Text(
-                    'Get LiveView Image',
-                    style: TextStyle(color: Colors.white),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'ISO 25600',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        SizedBox(height: settingsSpacer),
+                        Text(
+                          '180.00°',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        SizedBox(height: settingsSpacer),
+                        Text(
+                          'F2.8',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        SizedBox(height: settingsSpacer),
+                        Text(
+                          '5600K',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
+            ),
+            Expanded(
+              child: Stack(
+                alignment: Alignment.centerRight,
+                children: [
+                  if (imageBytes != null)
+                    Image.memory(
+                      imageBytes!,
+                      fit: BoxFit.contain,
+                      gaplessPlayback: true,
+                    ),
+                  MaterialButton(
+                    shape: const CircleBorder(),
+                    color: Colors.green,
+                    onPressed: _triggerRecord,
+                    child: const SizedBox(
+                      width: 72,
+                      height: 72,
+                    ),
+                  ),
+                  _buildActions()
+                ],
+              ),
             ),
           ],
         ),
