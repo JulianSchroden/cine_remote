99d2aee5c7012c82fcc910f96f6128b05a995a96
Julian Schroden <juschroden@gmail.com>
2022-12-28 23:25:28 +0100

    Declare color constants

---
+++ lib/presentation/cine_remote_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class CineRemoteColors {
+  const CineRemoteColors._();
+
+  static const accent = Color.fromARGB(255, 46, 55, 224);
+  static const accentForeground = Colors.white;
+
+  static const background = Color.fromARGB(255, 238, 238, 238);
+}
