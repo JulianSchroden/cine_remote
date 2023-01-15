c0ed3b0e844d541be051e3891b6c2de153faf202
Julian Schroden <juschroden@gmail.com>
2023-01-15 02:24:13 +0100

    Scroll to selected value when opening grid picker

--- lib/presentation/features/camera_control/widgets/control_prop_value_picker_grid.dart
+++ lib/presentation/features/camera_control/widgets/control_prop_value_picker_grid.dart
@@ -1,10 +1,12 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../domain/models/control_prop.dart';
 import '../bloc/props_control_cubit.dart';
 
-class ControlPropValuePickerGrid extends StatelessWidget {
+class ControlPropValuePickerGrid extends StatefulWidget {
   final ControlProp controlProp;
   final double maxWidth;
 
@@ -15,26 +17,83 @@ class ControlPropValuePickerGrid extends StatelessWidget {
   });
 
   @override
+  State<ControlPropValuePickerGrid> createState() =>
+      _ControlPropValuePickerGridState();
+}
+
+class _ControlPropValuePickerGridState
+    extends State<ControlPropValuePickerGrid> {
+  late final ScrollController _scrollController;
+  static const maxCrossAxisExtent = 80.0;
+  static const crossAxisSpacing = 16.0;
+  static const mainAxisSpacing = 16.0;
+  static const padding = EdgeInsets.all(16);
+
+  double calculateInitialScrollOffset() {
+    // Offset calculation based on [SliverGridDelegateWithMaxCrossAxisExtent] getLayout.
+    final crossAxisExtend = widget.maxWidth - padding.horizontal;
+    final int crossAxisCount =
+        (crossAxisExtend / (maxCrossAxisExtent + crossAxisSpacing)).ceil();
+    final double usableCrossAxisExtent = math.max(
+      0.0,
+      crossAxisExtend - (crossAxisSpacing * (crossAxisCount - 1)),
+    );
+    final double childCrossAxisExtent = usableCrossAxisExtent / crossAxisCount;
+    final double childMainAxisExtent = childCrossAxisExtent;
+
+    final itemIndex = widget.controlProp.allowedValues
+        .indexWhere((value) => value == widget.controlProp.currentValue);
+    final itemRowIndex = (itemIndex / crossAxisCount).floor();
+
+    return itemRowIndex * (childMainAxisExtent + mainAxisSpacing);
+  }
+
+  @override
+  void initState() {
+    _scrollController =
+        ScrollController(initialScrollOffset: calculateInitialScrollOffset());
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant ControlPropValuePickerGrid oldWidget) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (oldWidget.controlProp.type != widget.controlProp.type) {
+        _scrollController.jumpTo(calculateInitialScrollOffset());
+      }
+    });
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GridView(
       gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 80,
-        mainAxisSpacing: 16,
-        crossAxisSpacing: 16,
+        maxCrossAxisExtent: maxCrossAxisExtent,
+        mainAxisSpacing: mainAxisSpacing,
+        crossAxisSpacing: crossAxisSpacing,
       ),
-      padding: const EdgeInsets.all(16),
+      padding: padding,
+      controller: _scrollController,
       children: [
-        ...controlProp.allowedValues.map(
+        ...widget.controlProp.allowedValues.map(
           (value) {
-            final isCurrentValue = value == controlProp.currentValue;
+            final isCurrentValue = value == widget.controlProp.currentValue;
 
             return TextButton(
-              onPressed: controlProp.isPending
+              onPressed: widget.controlProp.isPending
                   ? null
                   : () {
                       context
                           .read<PropsControlCubit>()
-                          .setProp(controlProp.type, value);
+                          .setProp(widget.controlProp.type, value);
                     },
               style: ButtonStyle(
                 shape: MaterialStateProperty.all<OutlinedBorder>(
@@ -44,7 +103,7 @@ class ControlPropValuePickerGrid extends StatelessWidget {
                   isCurrentValue
                       ? BorderSide(
                           width: 4,
-                          color: controlProp.isPending
+                          color: widget.controlProp.isPending
                               ? Colors.grey
                               : Colors.green)
                       : null,
