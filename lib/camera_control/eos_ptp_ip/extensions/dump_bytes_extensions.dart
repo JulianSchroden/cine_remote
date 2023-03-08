93beeb5906749644b415746b4759e1fa4bc90e8d
Julian Schroden <juschroden@gmail.com>
2023-03-08 21:49:05 +0100

    Implement dumpHexData extension method on Uint8List

---
+++ lib/camera_control/eos_ptp_ip/extensions/dump_bytes_extensions.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+
+extension DumpHexDataExtension on Uint8List {
+  String dumpHexData() {
+    final hexValues =
+        map((value) => value.toRadixString(16).padLeft(2, '0')).toList();
+
+    final buffer = StringBuffer('\n');
+
+    var counter = 1;
+    for (var value in hexValues) {
+      buffer.write(value);
+
+      if (counter % 16 == 0) {
+        buffer.writeln();
+      } else if (counter % 8 == 0) {
+        buffer.write('  ');
+      } else {
+        buffer.write(' ');
+      }
+
+      counter++;
+    }
+
+    return buffer.toString();
+  }
+}
