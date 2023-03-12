c40e92327b42ef8a4b119cc7ab172ec02a0180fd
Julian Schroden <juschroden@gmail.com>
2023-03-12 23:56:52 +0100

    Move tests of get and set prop action to action tests

---
+++ test/camera_control/eos_cine_http/communication/set_prop_action_test.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:cine_remote/camera_control/eos_cine_http/communication/actions/set_prop_action.dart';
+import 'package:cine_remote/camera_control/eos_cine_http/constants/api_endpoint_path.dart';
+import 'package:cine_remote/camera_control/eos_cine_http/models/http_adapter_response.dart';
+import 'package:cine_remote/camera_control/interface/models/control_prop_type.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../camera_control_mocks.dart';
+
+void main() {
+  late MockHttpAdapter mockHttpAdapter;
+
+  setUp(() {
+    mockHttpAdapter = MockHttpAdapter();
+  });
+
+  test('should set iso value', () async {
+    when(
+      () => mockHttpAdapter.get(ApiEndpointPath.setProp, {'gcv': '1250'}),
+    ).thenAnswer((_) async => HttpAdapterResponse(
+          statusCode: 200,
+          jsonBody: jsonDecode('{"res":"ok"}'),
+          cookies: [],
+        ));
+
+    await SetPropAction(mockHttpAdapter, ControlPropType.iso, '1250').call();
+  });
+}
