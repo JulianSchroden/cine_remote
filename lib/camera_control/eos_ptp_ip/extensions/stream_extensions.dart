a4335b2ade7a721dd72f1dc2cd884dbca71c1841
Julian Schroden <juschroden@gmail.com>
2023-03-14 00:20:23 +0100

    Update Stream.firstWhereType extension to take optional predicate tot test

--- lib/camera_control/eos_ptp_ip/extensions/stream_extensions.dart
+++ lib/camera_control/eos_ptp_ip/extensions/stream_extensions.dart
@@ -1,6 +1,7 @@
-extension StreamFirstWhereTypeExtension<T> on Stream<T> {
-  Future<T> firstWhereType<T>() async {
-    final result = await firstWhere((element) => element is T);
+extension StreamFirstWhereTypeExtension<S> on Stream<S> {
+  Future<T> firstWhereType<T>([bool Function(T)? test]) async {
+    final result = await firstWhere(
+        (element) => element is T && (test?.call(element) ?? true));
     return result as T;
   }
 }
