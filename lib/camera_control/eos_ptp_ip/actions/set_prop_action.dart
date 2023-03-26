221297ef79df55cd18bdc81cdb46c33d4f1ad24b
Julian Schroden <juschroden@gmail.com>
2023-03-26 23:57:56 +0200

    Improve logging in setPropAction

--- lib/camera_control/eos_ptp_ip/actions/set_prop_action.dart
+++ lib/camera_control/eos_ptp_ip/actions/set_prop_action.dart
@@ -1,6 +1,7 @@
 import '../../interface/models/control_prop_type.dart';
 import '../communication/ptp_transaction_queue.dart';
 import '../constants/ptp_property.dart';
+import '../extensions/int_as_hex_string_extension.dart';
 import '../extensions/to_byte_extensions.dart';
 import '../models/eos_ptp_prop_value.dart';
 import 'action.dart';
@@ -14,13 +15,15 @@ class SetPropAction extends Action<void> {
   @override
   Future<void> run(PtpTransactionQueue transactionQueue) async {
     final propCode = mapPropTypeToCode(propType);
+    final nativeValue = propValue.nativeValue;
     if (propCode == null) {
       throw Error();
     }
 
-    logger.info('Setting property $propCode to ${propValue.nativeValue}');
+    logger.info(
+        'Setting property ${propCode.asHex()} to ${nativeValue.asHex()} ($nativeValue)');
 
-    final valueBytes = propValue.nativeValue.asUint32Bytes();
+    final valueBytes = nativeValue.asUint32Bytes();
     final setPropValue = operationFactory.createSetPropValue(
       propCode,
       valueBytes,
