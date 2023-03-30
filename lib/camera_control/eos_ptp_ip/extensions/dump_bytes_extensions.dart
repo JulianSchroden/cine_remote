a9da0ee859b683b7bd5b7ba42bda4521baa87ce0
Julian Schroden <juschroden@gmail.com>
2023-03-30 10:53:04 +0200

    Add option to display lineNumbers to dumpAsHex extension

--- lib/camera_control/eos_ptp_ip/extensions/dump_bytes_extensions.dart
+++ lib/camera_control/eos_ptp_ip/extensions/dump_bytes_extensions.dart
@@ -1,15 +1,23 @@
 import 'dart:typed_data';
 
-import '../../common/list_extensions.dart';
 import 'int_as_hex_string_extension.dart';
 
 extension DumpHexDataExtension on Uint8List {
-  String dumpAsHex({bool asValidList = false}) {
+  String dumpAsHex({
+    bool asValidList = false,
+    bool withLineNumbers = false,
+    int indentationCount = 3,
+  }) {
     final hexValues =
         map((value) => value.asHex(withPrefix: asValidList, padLeft: 2))
             .toList();
 
-    final buffer = StringBuffer('\n[\n   ');
+    var line = 0;
+    final indent = ' ' * indentationCount;
+    final startOfDump = withLineNumbers
+        ? '\n↓↓↓↓ [\n${_formatLineNumber(line)}$indent'
+        : '\n[\n$indent';
+    final buffer = StringBuffer(startOfDump);
 
     var counter = 1;
     for (var value in hexValues) {
@@ -19,8 +27,13 @@ extension DumpHexDataExtension on Uint8List {
       }
 
       if (counter % 16 == 0) {
-        if (counter != hexValues.lastIndex) {
-          buffer.write('\n   ');
+        line++;
+        if (counter != hexValues.length) {
+          final newLineStart = withLineNumbers
+              ? '\n${_formatLineNumber(line)}$indent'
+              : '\n$indent';
+
+          buffer.write(newLineStart);
         }
       } else if (counter % 8 == 0) {
         buffer.write('  ');
@@ -31,8 +44,12 @@ extension DumpHexDataExtension on Uint8List {
       counter++;
     }
 
-    buffer.write('\n]');
+    final endOfDump = withLineNumbers ? '\n↑↑↑↑ ]' : '\n]';
+    buffer.write(endOfDump);
 
     return buffer.toString();
   }
+
+  String _formatLineNumber(int line) =>
+      line.asHex(withPrefix: false, padLeft: 4);
 }
