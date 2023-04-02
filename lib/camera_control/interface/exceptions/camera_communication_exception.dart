90bd1890f51d0db6361419675b228a70bb1d1f24
Julian Schroden <juschroden@gmail.com>
2023-04-03 01:05:02 +0200

    Override toString for CameraCommunicationException

--- lib/camera_control/interface/exceptions/camera_communication_exception.dart
+++ lib/camera_control/interface/exceptions/camera_communication_exception.dart
@@ -2,4 +2,9 @@ class CameraCommunicationException implements Exception {
   final String message;
 
   const CameraCommunicationException(this.message);
+
+  @override
+  String toString() {
+    return 'CameraCommunicationException: $message';
+  }
 }
