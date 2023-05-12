d3bf95b79eba51d3b1fc6cf817e52870820162d7
Julian Schroden <juschroden@gmail.com>
2023-05-12 19:32:20 +0200

    Override toString for PtpOperationResponse

--- lib/camera_control/eos_ptp_ip/responses/ptp_operation_response.dart
+++ lib/camera_control/eos_ptp_ip/responses/ptp_operation_response.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import '../constants/ptp_response_code.dart';
+import '../extensions/int_as_hex_string_extension.dart';
 import 'ptp_response.dart';
 
 class PtpOperationResponse extends PtpResponse {
@@ -26,5 +27,10 @@ class PtpOperationResponse extends PtpResponse {
       transactionId ?? this.transactionId,
       data ?? this.data,
     );
+  }
+
+  @override
+  String toString() {
+    return "OperationResponse(responseCode: ${responseCode.asHex(padLeft: 2)}, transactionId: $transactionId, data.length: ${data.length})";
   }
 }
