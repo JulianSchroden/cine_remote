dd3b90abe7207119fe3bd08d255954bc1cbb0116
Julian Schroden <juschroden@gmail.com>
2023-05-13 00:37:27 +0200

    Update HttpAdapter to throw CommunicationAborted exceptions on timeout or client already closed

--- lib/camera_control/eos_cine_http/adapter/http_adapter.dart
+++ lib/camera_control/eos_cine_http/adapter/http_adapter.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:io';
 
+import '../../interface/exceptions/camera_communication_exception.dart';
 import '../extensions/client_response_extensions.dart';
 import '../models/http_adapter_response.dart';
 
@@ -7,11 +9,13 @@ class HttpAdapter {
   final HttpClient _client;
   final String _authority;
   final List<Cookie> _cookies = [];
+  bool _isClosed = false;
 
   HttpAdapter(this._client, this._authority);
 
   Future<void> close() async {
     _client.close();
+    _isClosed = true;
   }
 
   Future<HttpAdapterResponse> get(
@@ -24,12 +28,27 @@ class HttpAdapter {
   Future<HttpClientResponse> getRaw(
     String path, [
     Map<String, dynamic>? queryParams,
+    Duration timeLimit = const Duration(seconds: 2),
   ]) async {
-    final url = Uri.http(_authority, path, queryParams);
-    final request = await _client.getUrl(url);
-    request.cookies.addAll(_cookies);
+    if (_isClosed) {
+      throw const CameraCommunicationAbortedException(
+          'Failed to send request. Client  is closed');
+    }
 
-    return await request.close();
+    try {
+      final url = Uri.http(_authority, path, queryParams);
+      final request = await _client.getUrl(url).timeout(timeLimit);
+      request.cookies.addAll(_cookies);
+
+      return request.close().timeout(timeLimit);
+    } catch (e) {
+      if (e is SocketException || e is TimeoutException) {
+        throw const CameraCommunicationAbortedException(
+            'Failed to send request');
+      }
+
+      rethrow;
+    }
   }
 
   void addCookies(List<Cookie> cookies) {
