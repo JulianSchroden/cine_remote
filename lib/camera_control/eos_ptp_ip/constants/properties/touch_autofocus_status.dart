4e4d2c18907986724f692e80e9f46cffbcafce0c
Julian Schroden <juschroden@gmail.com>
2023-06-11 23:27:00 +0200

    Add faceTracking focusStatus

--- lib/camera_control/eos_ptp_ip/constants/properties/touch_autofocus_status.dart
+++ lib/camera_control/eos_ptp_ip/constants/properties/touch_autofocus_status.dart
@@ -1,12 +1,14 @@
 import '../ptp_property.dart';
 
 enum TouchAutofocusStatus {
+  faceTracking,
   tracking,
   focusFound,
   focusNotFound,
 }
 
 const List<EosEnumValue<TouchAutofocusStatus>> touchAutofocusValues = [
+  EosEnumValue(0x20, TouchAutofocusStatus.faceTracking),
   EosEnumValue(0x30, TouchAutofocusStatus.tracking),
   EosEnumValue(0x31, TouchAutofocusStatus.focusFound),
   EosEnumValue(0x32, TouchAutofocusStatus.focusFound),
